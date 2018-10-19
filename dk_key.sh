b83fe80c8079f5ff6bcbf41dfbbd3f830a72b739
nswiatec <nicholas@swiatecki.com>
2018-10-19 18:24:54 +0200

    added wget of file

--- dk_key.sh
+++ dk_key.sh
@@ -1,3 +1,5 @@
+echo "Hang on.."
+wget https://raw.githubusercontent.com/swiatecki/DevNetKeyboard/master/km-00000406.ini
 cp /etc/xrdp/km-00000400.ini /etc/xrdp/km-00000406.ini.bak
 cp km-00000406.ini /etc/xrdp/km-00000409.ini
 echo "Now reboot by typing 'sudo reboot'"
