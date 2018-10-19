1620394e00ec31b9e4d114a3d4bc7736de9f6e86
nswiatec <nicholas@swiatecki.com>
2018-10-19 19:42:38 +0200

    nicer v1

---
+++ nicer_dk_key.sh
@@ -0,0 +1,4 @@
+echo "Hang on.."
+wget https://raw.githubusercontent.com/swiatecki/DevNetKeyboard/master/km-00000406.ini
+cp km-00000406.ini /etc/xrdp/km-00000406.ini
+echo "Now reboot by typing 'sudo reboot'"
