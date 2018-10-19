143b6e1627f52cee2f47db17b691f2b7c2d1b42a
nswiatec <nicholas@swiatecki.com>
2018-10-19 19:01:02 +0200

    fixed typos

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # DevNetKeyboard
 
+This is a dirty hack, and it knows it...
+
 ```bash
-wget -O -h  "https://raw.githubusercontent.com/swiatecki/DevNetKeyboard/master/dk_key.sh" | sudo sh 
+wget -O - "https://raw.githubusercontent.com/swiatecki/DevNetKeyboard/master/dk_key.sh" | sudo sh 
+```
--- dk_key.sh
+++ dk_key.sh
@@ -1,5 +1,5 @@
 echo "Hang on.."
 wget https://raw.githubusercontent.com/swiatecki/DevNetKeyboard/master/km-00000406.ini
-cp /etc/xrdp/km-00000400.ini /etc/xrdp/km-00000406.ini.bak
+cp /etc/xrdp/km-00000409.ini /etc/xrdp/km-00000406.ini.bak
 cp km-00000406.ini /etc/xrdp/km-00000409.ini
 echo "Now reboot by typing 'sudo reboot'"
